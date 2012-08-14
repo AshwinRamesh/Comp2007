a93fbfbd806fc376ca1b66608e8ece050952113c
Ashwin Ramesh <ashwinramesh92@hotmail.com>
2012-08-14 21:28:53 +1000

    finishing off assignment2

--- Assignments/Assignment 2/Assignment2.py
+++ Assignments/Assignment 2/Assignment2.py
@@ -72,3 +72,26 @@ def DisconnectingPower():
                     G.size = G.size - child.size # remove all nodes below the child (inclusive) to get the larger forest
                 vertex.disconnectingPower = sum of the multiplication of each array element in tempArray x tempCount
     return G
+
+
+Simplified Algorithm:
+
+The idea behind this algorithm to run in O(n) time is to modify the DFS algoritm to calculate how many nodes are below every other node. This will keep the running time of O(m) as we are guaranteed that this is a connected graph.
+
+Now the cut verticies algorithm will use the result of the DFS and will take O(n^2) time to run. The idea of this algorithm is to flag all cut verticies. 
+
+The final peice is the actual algorithm to calculate the Disconnecting Power. This algorithm will iterate through every vertex in the Graph and then iterate through the children of that vertex to calculate the DP.
+The idea is to use the size attribute that we calculated in the DFS to make the process more effecient. This double iteration will ensure a n^2 running time.
+
+Overall the running time will be O(m) + O(n^2) + O(n^2) = O(n^2)
+
+
+Correctness:
+
+This algorith is correct because:
+    - The assumtion is that by removing one node, there will be one MAJOR forest above the removed node and many small forests below the node.
+    - Now some of the nodes below the removed node will have back edges.
+    - This means that parts of the smaller forests actually belong to the MAJOR forest.
+    - The size of the major forest is (n - all the nodes that take into account back edges)
+    - In the case that some nodes do not have back edges, then the size of the forest is the size attribute that we calculated during the DFS.
+    - So finally you will have a value for the major forest and many values for smaller forests. Multiply across and add for the disconnecting power.
